17e777123d5c1c1cff7197383d5217de5074ca64
Andresnicogcp <Andresnicogcpcurso@gmail.com>
2024-02-27 12:59:01 +0100

    novedades

--- frontend/src/app/components/pages/cart/cart.component.ts
+++ frontend/src/app/components/pages/cart/cart.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular'; // Asegúrate de importar IonicModule
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [IonicModule], // Asegúrate de agregar IonicModule aquí
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
